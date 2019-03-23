1994aaa6a85a0bba6407eec8a0d26f00dca949d7
sauloxd <saulotoshi@gmail.com>
2019-03-23 11:05:28 -0300

    extract env variables

--- scripts/s3-deploy.sh
+++ scripts/s3-deploy.sh
@@ -1,11 +1,11 @@
-#!bin/sh
+#!bin/bash
 BUCKET_NAME=saulo.dev
 
 ~/.local/bin/aws s3 sync build s3://$BUCKET_NAME \
-  --region=us-east-1 \
+  --region=$BUCKET_REGION \
   --delete
 
 ~/.local/bin/aws s3 cp s3://$BUCKET_NAME/index.html s3://$BUCKET_NAME/index.html \
   --metadata-directive REPLACE \
   --cache-control max-age=0 \
-  --region=us-east-1
+  --region=$BUCKET_REGION
